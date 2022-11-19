0e1205cec6069418094e5329398a6af66efa95ff
Parthiv7890 <88973956+Parthiv7890@users.noreply.github.com>
2022-11-19 16:36:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let objects = []
-let status = ""
+let status1 = ""
 let label = ""
 let confidence = 0
+let sound
 function setup(){
     canvas = createCanvas(600,420)
     canvas.center()
@@ -9,26 +10,28 @@ function setup(){
     video.size(600,420)
     video.hide()
     objDetector = ml5.objectDetector('cocossd',modelLoaded)
-    document.getElementById("status") = "Status : Detecting Objects"
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
 }
 function preload(){
-
+    song = loadSound('./christmas_bells.mp3')
 }
 function draw(){
     image(video,0,0,600,420)
-    if(status != ""){
+    if(status1 != ""){
         objDetector.detect(video,gotResults)
         for(i=0;i<objects.length;i++){
-            document.getElementById("status") = "Status : Objects Detected"
+            document.getElementById("status").innerHTML = "Status : Objects Detected"
             label = objects[i].label
             confidence = floor(objects[i].confidence * 100)
             if(label == 'person'){
                 document.getElementById("baby").innerHTML = "Baby Found"
+                song.stop()
             }else{
                 document.getElementById("baby").innerHTML = "Baby not Found"
+                song.play()
             }
             fill(255,0,0)
-            text(label + " "+confidencePercent+"% ",objects[i].x+5,objects[i].y+15)
+            text(label + " "+confidence+"% ",objects[i].x+5,objects[i].y+15)
             noFill()
             stroke(255,0,0)
             rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height)
@@ -36,7 +39,7 @@ function draw(){
     }
 }
 function modelLoaded(){
-    status ="true"
+    status1 ="true"
 }
 function gotResults(error,results){
     if(error){
